e80c940230cdd08921cf3ad0d8871813fae59922
Enoah Netzach <fabrizio.castellarin@prontopro.it>
2016-08-19 13:16:15 +0200

    add a waiting spinner

--- src/components/MovieDetails/Impl.js
+++ src/components/MovieDetails/Impl.js
@@ -2,6 +2,7 @@ import React, { PropTypes } from 'react'
 import { Link } from 'react-router'
 import FlatButton from 'material-ui/FlatButton'
 import NavigationArrowBack from 'material-ui/svg-icons/navigation/arrow-back'
+import CircularProgress from 'material-ui/CircularProgress'
 import { searchRoute } from '../Routes'
 import Details from './Details'
 
@@ -14,7 +15,13 @@ const Impl = ({ movie }) => (
       />
     </Link>
 
-    {movie ? <Details movie={movie} /> : null}
+    {movie ? (
+      <Details movie={movie} />
+    ) : (
+      <div style={{ margin: '0 auto', textAlign: 'center' }}>
+        <CircularProgress />
+      </div>
+    )}
   </div>
 )
 
